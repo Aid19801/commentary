21136888fb7df3c8773ee03c994053d688d85a2d
Adrian Thompson <adrian.thompson@sky.uk>
2018-10-17 22:15:32 +0100

    added keymoments section, data coming in

--- src/containers/key-moments/key-moments.js
+++ src/containers/key-moments/key-moments.js
@@ -5,13 +5,41 @@ import './styles.css';
 class KeyMoments extends React.Component {
     constructor() {
         super();
-        this.state = {}
+        this.state = {
+            keyMoments: [],
+        }
     }
-    render() {
-        return (
-            <div className="key-moments">
-                <h1 className="key-moments-title">key moments</h1>
 
+    processMoments = (np) => {
+        const { data } = np;
+        let filteredToKeyMoments = data.filter((each) => each.isKeyMoment === true);
+
+        this.setState({
+            keyMoments: filteredToKeyMoments,
+        })
+    }
+    
+    componentWillReceiveProps(nextProps) {
+        this.processMoments(nextProps)
+    }
+
+    render() {
+
+        const { keyMoments } = this.state;
+        console.log('THIS state: ', keyMoments);
+        return (
+            <div className="key-moments-container">
+                <h1 className="key-moments-title">key moments</h1>
+                <div className="key-moments-section">
+                    { keyMoments.map((each, i) => {
+                        return (
+                            <div className="key-moments-row">
+                                <h2>{each.playerNumber}</h2>
+                                <h4>{each.keyMomentText}</h4>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         )
     }
@@ -22,7 +50,7 @@ const mapStateToProps = (state) => ({
 })
 
 // const mapDispatchToProps = (dispatch) => ({
-//     data: state.updatesReducer.data,
+//     data: () => dispatch({ type: xxxx })
 // })
 
 export default connect(mapStateToProps, null)(KeyMoments);
